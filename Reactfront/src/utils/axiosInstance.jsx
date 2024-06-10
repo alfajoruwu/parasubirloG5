fffe07c4e51ddf07e9ae23a492593b7d7ef50716
alfajoruwu <mcamilla20@alumnos.utalca.cl>
2024-06-09 23:10:06 -0400

    fix2

--- Reactfront/src/utils/axiosInstance.jsx
+++ Reactfront/src/utils/axiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 import authService from './authService'
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000/',
+  baseURL: 'django:8000/',
   withCredentials: true
 })
 
