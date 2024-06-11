f713026cf8e82e272d02d9a0eeb94150d52f146d
alfajoruwu <mcamilla20@alumnos.utalca.cl>
2024-06-10 23:46:58 -0400

    xd

--- DjangoApi/api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Modulo, Postulacion, Oferta, Resolucion
-
-# Register your models here.
-admin.site.register(Modulo)
-admin.site.register(Postulacion)
-admin.site.register(Oferta)
-admin.site.register(Resolucion)
