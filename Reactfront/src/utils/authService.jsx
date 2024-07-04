48dec7d237348171f1d6f46e6535c4a86f9cfcfa
alfajoruwu <mcamilla20@alumnos.utalca.cl>
2024-07-04 11:35:37 -0400

    ip

--- Reactfront/src/utils/authService.jsx
+++ Reactfront/src/utils/authService.jsx
@@ -3,7 +3,7 @@ import Cookies from 'js-cookie'
 import { jwtDecode } from 'jwt-decode'
 import dayjs from 'dayjs'
 
-const API_URL = 'http://localhost:8000/token/'
+const API_URL = 'http://34.176.76.5:8000/token/'
 const REFRESH_URL = `${API_URL}refresh/`
 
 let isRrefreshing = false
